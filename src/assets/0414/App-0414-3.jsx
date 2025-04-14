42862146beff7da158c2b5211c0a1d73a70a32bb
HUANG YI NING-CODE <a0960102890@gmail.com>
2025-04-14 12:54:54 +0800

    React0414

---
+++ src/assets/0414/App-0414-3.jsx
@@ -0,0 +1,50 @@
+// 子元件
+function MyComponent(props) {
+//    查看props物件
+    console.log(props)
+
+    // 執行父元件傳送過來的c屬性函式
+    props.c()
+
+    // 使用大括號{props.屬性名稱}
+    return <>
+        <div>我是子元件，帶有主元件傳遞過來的屬性內容為「{props.a}」</div>
+        <div>我是子元件，帶有主元件傳遞過來的屬性內容為「{props.b}」</div>
+        {props.c()}  
+    </>
+
+}
+// 子元件
+// 由於props是物件，所以可以使用解構賦值，來取用資料
+function MyComponent2({a,b,c}) {
+    // 呼叫c屬性的方法
+  
+    return <>
+        <div>第一個屬性{a} </div>
+        <div>第一個屬性{b} </div>
+        <div>第一個屬性{c()} </div>
+        <div>第一個屬性{console.log(d)} </div>            
+    </>
+}
+
+// 父元件
+function App() {
+    return (
+        <>
+            {/* 回條函式=>callback function */}
+            <MyComponent
+                a="我是屬性a"
+                b="我是屬性b"
+                c={() => { console.log("我是屬性c") }}
+            />
+
+            <MyComponent2
+                a="我是屬性a"
+                b="我是屬性b"
+                c={() => { console.log("我是屬性c") }}
+            />
+
+        </>
+    )
+}
+export default App
