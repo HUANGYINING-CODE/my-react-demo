00f0a06d88da875e06c1ddddb73fe7f41f547b55
HUANG YI NING-CODE <a0960102890@gmail.com>
2025-04-16 11:06:15 +0800

    0408-3

---
+++ src/0408/App-0408-1.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1>Hi, React</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat quisquam voluptatem optio voluptatibus asperiores natus dolore maiores atque adipisci odio necessitatibus culpa ab nemo, ducimus deserunt? Eum, alias accusamus?</p>
+        </>
+    )
+}
+export default App;
